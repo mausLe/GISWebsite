55b37f07eb4e113282d25bad14a77ebc1ca9261b
mausLe <anhlt.cse@gmail.com>
2020-12-13 16:41:05 +0700

    Adding database model

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # GISWebsite
-This project is a project of IE402.L11 at VNUHCM-UIT.
+This repo is a project of IE402.L11 at VNUHCM-UIT.
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    userID = models.IntegerField(primary_key=True)
+    userName = models.CharField(max_length=20, null=True)
+    age = models.IntegerField(null=True)
+    address = models.CharField(max_length=50, null=True)
+    phone = models.CharField(max_length=15, null=True)
+    dateCreated = models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__ (self):
+        return self.userName
+
+class TrackingDevice (models.Model):
+    # Provide some devices id to refer
+    deviceID = models.IntegerField(primary_key=True)
+    deviceName = models.CharField(max_length=30, null=True)
+    mfd = models.DateTimeField(auto_now_add=True, null=True)
+    warranty = models.DateTimeField(auto_now_add=True, null=True)
+
+    userid = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class TrackingData (models.Model):
+    dataID = models.IntegerField(primary_key=True)
+    longitude = models.FloatField(null=True)
+    lattitude = models.FloatField(null=True)
+    timeRecorded = models.DateTimeField(auto_now_add=True, null=True)
+    deviceid = models.ForeignKey(TrackingDevice, on_delete=models.CASCADE)
+
+
