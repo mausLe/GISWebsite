7e5bece64b899cc9f87bc7351c33491cec2fe644
mausLe <anhlt.cse@gmail.com>
2020-12-20 17:54:14 +0700

    Adding reload Using jQuery()
    Follow these instrutions: https://crunchify.com/how-to-refresh-div-content-without-reloading-page-using-jquery-and-ajax/

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 import os, folium
+import turtle
 
 # Create your views here.
 def createMap():
     m = folium.Map(location = [10.869800, 106.803000], zoom_start = 20)
+
+    feature_group = folium.FeatureGroup(name="my group")
+
+    folium.Marker(location = [10.869800, 106.803000], 
+    popup="My location",
+    icon=folium.Icon(icon="globe")
+    ).add_to(m)
 
     m = m._repr_html_()
     context = {"my_map": m}
@@ -15,6 +23,8 @@ def createMap():
 def home(request):
     
     return location(request)
+
+
 
 def location(request):
 
@@ -28,4 +38,6 @@ def history(request):
     # Load database to mark user location
     context = createMap()
 
-    return render( request, "accounts/history.html", context)
+    # return render( request, "accounts/history.html", context)
+    return render( request, "accounts/navbar.html", context)
+
