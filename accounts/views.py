8c8c7ab065b077f4de0b865fbf4ef25dc8ea8b72
mausLe <anhlt.cse@gmail.com>
2021-01-05 16:09:40 +0700

    Merge SSHTunnelForwarder to History mode

--- accounts/views.py
+++ accounts/views.py
@@ -58,11 +58,6 @@ with SSHTunnelForwarder(
     # trans = numpy_array.transpose()
     # results = trans.tolist()
 
-"""
-    new_coord[:, 0] = coord[:, 0]//1000000 + coord[:, 0]%1000000/1000000
-    new_coord[:, 1] = coord[:, 1]//1000000 + coord[:, 1]%1000000/1000000
-"""
-
 my_coord = []
 for item in numpy_array:
     a = item[2]/1000000
@@ -74,42 +69,10 @@ for item in numpy_array:
 # Create your views here.
 def createMap():
     m = folium.Map(
-        location = [10.869800, 106.803000],
-        zoom_start = 20)
+        location = my_coord[0],
+        zoom_start = 16)
     
-    coordinates=[(10.869372, 106.802441),(10.870220, 106.802323),(10.870531, 106.802017),(10.871734, 106.802795),(10.873451, 106.802814)]
-    coordinates = my_coord
-    #a = [float(i) for i in results[2]]
-    #b = [float(i) for i in results[3]]
-    #coordinates = np.array(a/1000000, b/1000000)
-    #coordinates = (float(results[2])/1000000, float(results[3])/1000000)
-    for i in range(len(coordinates)):
-        t = results[i][4].strftime('%d/%m/%y')
-        seconds = results[i][5].total_seconds()
-        seconds = seconds % (24 * 3600)
-        hour = seconds // 3600
-        seconds %= 3600
-        minutes = seconds // 60
-        seconds %= 60
-        h = "%d:%02d:%02d" % (hour, minutes, seconds)
-        folium.Marker(location = coordinates[i],
-                      icon=folium.Icon(icon="globe")
-                      ).add_to(m)
-
-        #folium.Marker(location = coordinates[1],
-        #             popup="My first location",
-        #             icon=folium.Icon(icon="globe")
-        #             ).add_to(m)
-
-    #for each in coordinates:
-    #   folium.Marker(each).add_to(m)
-
-    folium.PolyLine(coordinates, color="red", weight=2.5, opacity=1).add_to(m)
-
-    m = m._repr_html_()
-    context = {"my_map": m}
-
-    return context
+    return m
 
 def home(request):
 
@@ -120,15 +83,44 @@ def home(request):
 def location(request):
 
     # read real time data
-    context = createMap()
+    map = createMap()
+    
+    folium.Marker(location = [10.869800, 106.803000], 
+    popup="My location",
+    icon=folium.Icon(icon="globe")
+    ).add_to(map)
+
+    map = map._repr_html_()
+    context = {"my_map": map}
+
+    # threading.Timer(WAIT_SECONDS, location(request)).start()
 
     return render( request, "accounts/location.html", context)
 
 
 def history(request):
-    # Load database to mark user location
-    context = createMap()
+    # coordinates=[(10.869372, 106.802441),(10.870220, 106.802323),(10.870531, 106.802017),(10.871734, 106.802795),(10.873451, 106.802814)]
+    coordinates = my_coord
+    map = createMap()
+    folium.Marker(coordinates[0], popup="Starting point", icon=folium.Icon(color="green", icon="star")).add_to(map)
+    folium.Marker(coordinates[-1], popup="Destination", icon=folium.Icon(color="red", icon="globe")).add_to(map)
 
-    # return render( request, "accounts/history.html", context)
+
+    for i in range(len(coordinates)):
+        t = results[i][4].strftime('%d/%m/%y')
+        seconds = results[i][5].total_seconds()
+        seconds = seconds % (24 * 3600)
+        hour = seconds // 3600
+        seconds %= 3600
+        minutes = seconds // 60
+        seconds %= 60
+        h = "%d:%02d:%02d" % (hour, minutes, seconds)
+        folium.CircleMarker(location = coordinates[i], radius=10, popup = (h, t)).add_to(map)
+
+    folium.PolyLine(coordinates, color="red", weight=2.5, opacity=1).add_to(map)
+
+    map = map._repr_html_()
+    context = {"my_map": map}
+
     return render( request, "accounts/navbar.html", context)
 
